c1c36d187e1706d6d86691ce27fd2cc010843d10
manokimbr <kim.souza.2201@gmail.com>
2021-10-19 19:36:25 -0300

    user access token fetching and authorization

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+/** 
+ * Receber code (string)
+ * Recuperar o access_token no github
+ * Recuperar infos do user no github
+ * Verificar se o usuario existe no DB
+ * ----SIM = Geram um token
+ * ----NÃO = Cria no DB, gera um token
+ * Retornar o token com as infos do user
+*/
+
+interface IAccessTokenResponse {
+    access_token: string 
+}
+
+interface IUserResponse {
+    avatar_url: string,
+    login: string,
+    id: number,
+    name: string
+}
+
+class AuthenticateUserService {
+    async execute(code: string) {
+        const url = "https://github.com/login/oauth/access_token"
+
+        const { data :accessTokenResponse } = await axios.post<IAccessTokenResponse>(url,null, {
+            params: {
+                client_id: process.env.GITHUB_CLIENT_ID,
+                client_secret: process.env.GITHUB_CLIENT_SECRET,
+                code,
+            },
+            headers: {
+                "Accept" : "application/json"
+            },
+        });
+
+        const response = await axios.get<IUserResponse>("https://api.github.com/user", {
+            headers: {
+                authorization: `Bearer ${accessTokenResponse.access_token}`
+            }
+        })
+
+        return response.data;
+    }
+}
+
+
+export { AuthenticateUserService }
