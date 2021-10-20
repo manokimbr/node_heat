a525b2b2dda5429d21593401b774d0d8ad2f5d1e
manokimbr <kim.souza.2201@gmail.com>
2021-10-19 22:07:29 -0300

    create message

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { ensureAuthenticaded } from "./middleware/ensureAuthenticated";
 
 const router = Router();
 
-router.post("/authenticate", new AuthenticateUserController().handle)
+router.post("/authenticate", new AuthenticateUserController().handle);
 
+router.post(
+  "/messages",
+  ensureAuthenticaded,
+  new CreateMessageController().handle
+);
 
-export { router }
+export { router };
